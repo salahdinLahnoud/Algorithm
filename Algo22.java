b6e58098d104993f6f68960481cc6cd1eb9bfd82
Salahdin Lahnoud <0775720@hr.nl>
2016-11-21 19:07:33 +0100

    Add files via upload

---
+++ Algo22.java
@@ -0,0 +1,12 @@
+import com.sun.xml.internal.bind.v2.runtime.output.StAXExStreamWriterOutput;
+
+public class Algo22 {
+	public static void main(String [] args){
+		
+		
+		System.out.println("Test Git");
+		
+		
+	}
+
+}
